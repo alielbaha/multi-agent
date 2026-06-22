4a9101c7f533d9aec65ddfb51cd4a737fd0bd1a0
unknown <68299151+alielbaha@users.noreply.github.com>
2026-06-22 23:23:18 +0200

    added post request for action recommendation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-
+from app.schemas.recommendation import RecommendationRequest
 app = FastAPI(title="multi agent system")
 
 
 @app.get("/health")
 def health():
     return {"status": "fine"}
+
+
+@app.post("/recommend")
+def recommand(request: RecommendationRequest):
+    return {
+        "ticker": request.ticker,
+        "signal": "BUY",
+    }
