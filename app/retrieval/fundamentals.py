190a9c67c86deff8dc9268419b31173049ce97c5
unknown <68299151+alielbaha@users.noreply.github.com>
2026-06-25 00:40:30 +0200

    created fundamentals to get data from yf

---
+++ app/retrieval/fundamentals.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+
+def get_fundamentals(ticker:str):
+    stock = yf.Ticker(ticker)
+    info = stock.info
+
+    return {
+        "pre_ratio": info.get("trailingPE"),
+        "market_cap": info.get("marketCap"),
+        "revenue_growth": info.get("revenueGrowth"),
+        "profit_margins": info.get("profitMargins"),
+        "debt_to_equity": info.get("debtToEquity")
+    }
